35106023b8c1571980f74b5d7b9fe169d01e0222
Karolina Trachtova <k.trachtova@gmail.com>
2026-09-03 15:34:29 +0200

    Fixing apptainer java bug

--- bin/02_preprocessing_truseq.sh
+++ bin/02_preprocessing_truseq.sh
@@ -5,6 +5,9 @@
 #
 set -euo pipefail
 
+# Prevent Java hsperfdata conflicts when running inside containers
+export JAVA_TOOL_OPTIONS="-XX:+PerfDisableSharedMem"
+
 SAMPLE=$1
 THREADS=$2
 
@@ -68,7 +71,7 @@ echo "####################################################"
 
 echo "$(date +%s) Started collapsing reads for sample $SAMPLE"
 
-gunzip -c ./intermediate_files/adapter1_trim/$(basename $SAMPLE .fastq.gz).ad3trim.fastq.gz | fastx_collapser -Q$QUALITY | reformat.sh qfake=40 in=stdin.fa out=stdout.fq > $(basename $SAMPLE .fastq.gz).cleaned.fastq
+gunzip -c ./intermediate_files/adapter1_trim/$(basename $SAMPLE .fastq.gz).ad3trim.fastq.gz | fastx_collapser -Q$QUALITY | reformat.sh qfake=40 in=stdin.fa out=$(basename $SAMPLE .fastq.gz).cleaned.fastq
 
 echo "$(date +%s) Finished collapsing sample $SAMPLE"
 
